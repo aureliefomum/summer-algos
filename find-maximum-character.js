8b70b0c633ed2a8a24c8c3d3217a66ed1da4c181
aureliefomum <azam_aurelie@yahoo.com>
2021-08-11 15:17:10 -0300

    find most repeated character

--- find-maximum-character.js
+++ find-maximum-character.js
@@ -7,6 +7,30 @@
 // });
 
 // SOLUTION:
+//1)
+function maxCharacter(str){
+    let charKeys ={}
+    
+    for (let char of str){
+      if(charKeys[char]){
+        charKeys[char]++
+        }else{
+          charKeys[char]=1
+         }   
+      }
+   let maxNum = 0
+   let maxChar= ''
+
+   for (let x in charKeys){
+     if(charKeys[x] > maxNum) {
+         maxNum = charKeys[x]
+         maxChar = x
+     }
+     }
+     return maxChar
+   }
+
+   
 //2)
 function maxCharacter(str){
     let charKeys ={}
