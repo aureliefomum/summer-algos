a86be68bd9213f10ee09a620a9e170f66d7a8aff
aureliefomum <azam_aurelie@yahoo.com>
2021-08-11 15:05:08 -0300

    find most repeated character

---
+++ find-maximum-character.js
@@ -0,0 +1,9 @@
+// Given a string of characters, return the character that appears the most often.
+
+// describe("Max Character", () => {
+// it("Should return max character", () => {
+//   assert.equal(max("Hello World!"), "l");
+// });
+// });
+
+// SOLUTION:
