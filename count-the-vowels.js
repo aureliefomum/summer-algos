c6ffea95f8ea4cf7bde240f345f311aaaa2ff2d4
aureliefomum <azam_aurelie@yahoo.com>
2021-08-13 10:12:15 -0300

    count vowels

---
+++ count-the-vowels.js
@@ -0,0 +1,21 @@
+/*
+Given a string of words or phrases, count the number of vowels.
+
+No Regex! Unless that is your brute force...
+
+Parameters
+Will receive a string of words or phrases
+
+Returns
+Will return the number of vowels added up 
+
+Examples:
+IN: "banana"
+OUT: 3
+IN: "banana, apple"
+OUT: 5
+*/
+
+    
+
+
