9f3218c2146c3352469f8520832abff93b8b760b
aureliefomum <azam_aurelie@yahoo.com>
2021-08-24 20:37:44 -0300

    reverse an array

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -11,3 +11,12 @@ return newArr
 }
 
 
+function reverseArr(arr){
+  for (let i =0; i<arr.length/2;i++){
+      let temp=arr[i]
+      arr[i]= arr[arr.length-1-i]
+      arr[arr.length-1-i] = temp
+  }
+  return arr
+
+}
