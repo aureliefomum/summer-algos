9ca03a0b305fb42056747a58571352d371411597
aureliefomum <azam_aurelie@yahoo.com>
2021-08-31 15:40:46 -0300

    fibonacci number

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -21,3 +21,12 @@
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 // From: https://leetcode.com/problems/fibonacci-number/
 
+//SOLUTION:
+function fib (n){
+
+    let arr=[0,1]
+    for(let i=2 ; i<=n; i++){
+        arr[i]= arr[i-1] + arr[i-2]
+    }
+   return arr[n]
+}
