1f54ee38f5a05ab7042cfc910afe1e39bfbc7532
aureliefomum <azam_aurelie@yahoo.com>
2021-09-07 12:08:06 -0300

    two sum

--- two-sum.js
+++ two-sum.js
@@ -6,4 +6,22 @@
 //  });
 // });
 
+// SOLUTION:
+// twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]
+const twoSum = (array, sum) => {
+  const pairs = [];
+  const store = {}
 
+  for (let part1 of array) {
+    const part2 = sum - part1;
+    // if (store.indexOf(part2) !== -1){
+    //    pairs.push([part1, part2]);
+    // }
+    if(!store[part2]){
+      store[part1] = 1
+    }else{
+      pairs.push([part1,part2])
+    }
+  }
+  return pairs;
+};
