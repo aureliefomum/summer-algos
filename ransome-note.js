18f1ed26132d94fb05fa4a8932c2593e58016d1c
aureliefomum <azam_aurelie@yahoo.com>
2021-09-30 23:06:06 -0300

    ransom note

--- ransome-note.js
+++ ransome-note.js
@@ -52,5 +52,25 @@ function ransom(note, magazine){
       return possible
   }
 
+//2)
+const canConstruct = function (ransomNote, magazine) {
+  const map = new Map();
 
+  for (let i = 0; i < magazine.length; i++) {
+    if (map.has(magazine[i])) {
+      map.set(magazine[i], map.get(magazine[i]) + 1)
+    } else {
+      map.set(magazine[i], 1)
+    }
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (!map.has(ransomNote[i]) || map.get(ransomNote[i]) === 0) {
+      return false;
+    }
+    map.set(ransomNote[i], map.get(ransomNote[i]) - 1)
+  }
+
+  return true;
+};
   
